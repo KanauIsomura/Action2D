1f72248c3bf0f55225e8121bc156ee805105af63
KanauIsomura <iso.kanau627@gmail.com>
2018-03-19 16:21:46 +0900

    アクションゲーム用キャラクターベースを追加

---
+++ Assets/Scripts/Character/CharacterOperationBase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Iso.Character {
+	public abstract class CharacterOperationBase : MonoBehaviour {
+		[SerializeField, Tooltip("操作管理スクリプト、同じオブジェクトについている場合はセットしなくていい")]
+		protected CharacterOperatorBase m_OperatorScript;                                //操作管理スクリプトを持っておく
+		[SerializeField, Tooltip("処理に入っていい状態のリスト")]
+		protected List<string>          m_StartProcessingStateList = new List<string>(); //処理に入っていい状態のリスト
+		[SerializeField, Tooltip("処理に入ったときの自分の状態")]
+		protected string                m_MyState;
+
+		void Awake() {
+			if(m_OperatorScript != null)
+				return;
+			m_OperatorScript = GetComponent<CharacterOperatorBase>();
+			m_StartProcessingStateList.Add(m_MyState);
+		}
+
+		void Update() {
+			string State = m_OperatorScript.GetStateName();
+			for(int nCount = 0; nCount < m_StartProcessingStateList.Count; ++nCount) {
+				//処理に入っていい状態か
+				if(State == m_StartProcessingStateList[nCount])
+					break;//確認終了して操作処理へ
+				//処理に入っていい状態が見つからずに最後まで末尾に到達したか
+				if(nCount == m_StartProcessingStateList.Count - 1)
+					return;//処理から抜ける
+			}
+
+			//操作処理
+			Operation();
+		}
+
+		/// <summary>
+		/// 操作関数、継承側はこれに上書きをする
+		/// </summary>
+		protected abstract void Operation();
+	}
+}
---
+++ Assets/Scripts/Character/CharacterOperatorBase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Iso.Character {
+	public class CharacterOperatorBase : MonoBehaviour {
+		[SerializeField] string m_StateName;     //現在の状態
+		string m_DefaultState;  //Defaultの状態
+
+		// Use this for initialization
+		void Awake () {
+			m_StateName    = "None";
+			m_DefaultState = "None";
+		}
+		
+		/// <summary>
+		/// 状態をDefaultの状態へ
+		/// </summary>
+		public void ChangeDefault() {
+			m_StateName = m_DefaultState;
+		}
+
+		/// <summary>
+		/// 状態を変化させる
+		/// </summary>
+		/// <param name="Name">変更したい状態名</param>
+		public void SetStateName(string Name) {
+			m_StateName = Name;
+		}
+
+		/// <summary>
+		/// Defaultの状態を設定
+		/// </summary>
+		/// <param name="Name">Default状態の名前</param>
+		public void SetDefaultState(string Name) {
+			m_StateName = m_DefaultState = Name;
+		}
+
+		/// <summary>
+		/// 現在の状態を取得
+		/// </summary>
+		/// <returns></returns>
+		public string GetStateName() {
+			return m_StateName;
+		}
+	}
+}
